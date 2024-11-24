df2fbbdcf2b89ff68d8cbaf18e9a3855a45348da
Hatsune Miku <woaixkzero@gmail.com>
2024-11-24 21:31:02 +0800

    loadVideoList in user_center and VideoStatusCount

--- easylive-java/easylive-web/src/main/java/com/easylive/web/controller/UcenterVideoPostController.java
+++ easylive-java/easylive-web/src/main/java/com/easylive/web/controller/UcenterVideoPostController.java
@@ -1,9 +1,13 @@
 package com.easylive.web.controller;
 
 import com.easylive.entity.dto.TokenUserInfoDto;
+import com.easylive.entity.enums.VideoStatusEnum;
 import com.easylive.entity.po.VideoInfoFilePost;
 import com.easylive.entity.po.VideoInfoPost;
+import com.easylive.entity.query.VideoInfoPostQuery;
+import com.easylive.entity.vo.PaginationResultVO;
 import com.easylive.entity.vo.ResponseVO;
+import com.easylive.entity.vo.VideoStatusCountVO;
 import com.easylive.service.VideoInfoFilePostService;
 import com.easylive.service.VideoInfoPostService;
 import com.easylive.service.VideoInfoService;
@@ -60,5 +64,53 @@ public class UcenterVideoPostController extends ABaseController{
         videoInfoPostService.saveVideoInfo(videoInfoPost, filePostList);
 
         return getSuccessResponseVO(null);
+    }
+
+    //加载用户稿件
+    @RequestMapping("/loadVideoList")
+    public ResponseVO loadVideoPost(Integer status, Integer pageNo, String videoNameFuzzy){
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+        VideoInfoPostQuery videoInfoQuery = new VideoInfoPostQuery();
+        videoInfoQuery.setUserId(tokenUserInfoDto.getUserId());
+        videoInfoQuery.setOrderBy("v.create_time desc");
+        videoInfoQuery.setPageNo(pageNo);
+        if(status!=null){
+            //状态-1:审核进行中的稿件
+            if(status==-1){
+                //排除审核成功的稿件和审核不通过的稿件
+                videoInfoQuery.setExcludeStatusArray(new Integer[]{VideoStatusEnum.STATUS3.getStatus(),VideoStatusEnum.STATUS4.getStatus()});
+            }else {
+                videoInfoQuery.setStatus(status);
+            }
+        }
+        videoInfoQuery.setVideoNameFuzzy(videoNameFuzzy);
+        videoInfoQuery.setQueryCountInfo(true);
+        PaginationResultVO resultVO = videoInfoPostService.findListByPage(videoInfoQuery);
+
+        return getSuccessResponseVO(resultVO);
+    }
+
+    //用户的稿件中各个状态的稿件的数量获取
+    @RequestMapping("/getVideoCountInfo")
+    public ResponseVO getVideoCountInfo(){
+        TokenUserInfoDto tokenUserInfoDto = getTokenUserInfoDto();
+
+        VideoInfoPostQuery videoInfoPostQuery = new VideoInfoPostQuery();
+        videoInfoPostQuery.setUserId(tokenUserInfoDto.getUserId());
+        //审核成功的视频
+        videoInfoPostQuery.setStatus(VideoStatusEnum.STATUS3.getStatus());
+        Integer auditPassCount = videoInfoPostService.findCountByParam(videoInfoPostQuery);
+        //审核失败的视频
+        videoInfoPostQuery.setStatus(VideoStatusEnum.STATUS4.getStatus());
+        Integer auditFailCount = videoInfoPostService.findCountByParam(videoInfoPostQuery);
+        //审核进行中的视频
+        videoInfoPostQuery.setStatus(null);
+        videoInfoPostQuery.setExcludeStatusArray(new Integer[]{VideoStatusEnum.STATUS3.getStatus(),VideoStatusEnum.STATUS4.getStatus()});
+        Integer inProgress = videoInfoPostService.findCountByParam(videoInfoPostQuery);
+        VideoStatusCountVO countVO = new VideoStatusCountVO();
+        countVO.setAuditPassCount(auditPassCount);
+        countVO.setAuditFailCount(auditFailCount);
+        countVO.setInProgress(inProgress);
+        return getSuccessResponseVO(countVO);
     }
 }
