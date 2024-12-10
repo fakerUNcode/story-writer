bd89018dc5d4eccdc28cb57700e2fc0f080b3af7
Hatsune Miku <woaixkzero@gmail.com>
2024-12-10 16:49:45 +0800

    implement actualTimeStatistics and lastWeekStatistics

--- easylive-java/easylive-common/src/main/java/com/easylive/utils/DateUtil.java
+++ easylive-java/easylive-common/src/main/java/com/easylive/utils/DateUtil.java
@@ -5,10 +5,9 @@ import com.easylive.entity.enums.DateTimePatternEnum;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class DateUtil {
 
@@ -48,10 +47,21 @@ public class DateUtil {
         return new Date();
     }
 
+    //只返回单个日期。例如，假设今天是2024-12-10，如果传入 3，只会返回"2024-12-07"
     public static String getBeforeDayDate(Integer day){
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_YEAR,-day);
         return format(calendar.getTime(), DateTimePatternEnum.YYYY_MM_DD.getPattern());
     }
 
+   // 会生成一个日期序列。例如，假设今天是2024-12-10，如果传入 3，会返回["2024-12-07", "2024-12-08", "2024-12-09"]
+    public static List<String> getBeforeDaysDate(Integer beforeDays) {
+        LocalDate endDate = LocalDate.now();
+        List<String> dateList = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for(int i = beforeDays; i>0; i--){
+            dateList.add(endDate.minusDays(i).format(formatter));
+        }
+        return dateList;
+    }
 }
